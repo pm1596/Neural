ab0713b33c1c42a19ac269512bb746b4b73940f5
Pranjal Mendhekar <pm1596@yahoo.com>
2018-09-17 09:50:18 +0530

    Update parameters_n_input.py

--- parameters_n_input.py
+++ parameters_n_input.py
@@ -1,9 +1,9 @@
 #change
 # Parameters
-learning_rate = 0.001
-training_iters = 100000
+learning_rate = 0.01
+training_iters = 1000
 #batch_size = 100
-display_step = 10
+display_step = 50
 
 # Network Parameters
 no_input = 1024 # matrix dataset input (adj_mat shape: 32*32)
